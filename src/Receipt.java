1da790d20a83b0b494dba99b5d58501bdc09d871
enjelitabrlingga <155749013+enjelitabrlingga@users.noreply.github.com>
2025-01-13 05:10:05 +0700

    Update Receipt.java

--- src/Receipt.java
+++ src/Receipt.java
@@ -1,16 +1,28 @@
-public class Receipt {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class Receipt {
     private Cart cart;
 
     public Receipt(Cart cart) {
         this.cart = cart;
     }
 
-    public String generateReceipt() {
-        StringBuilder receipt = new StringBuilder("Struk Pembelian:\n");
-        for (MenuItem item : cart.getItems()) {
-            receipt.append(item.getName()).append(" - Rp ").append(item.getPrice()).append("\n");
+    public void generateReceipt(String paymentMethod) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("receipt.txt", true))) {
+            writer.println("--- Struk Pembelian ---");
+            writer.println("Tanggal: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            for (MenuItem item : cart.getItems()) {
+                writer.println(item.getName() + " - Rp " + item.getPrice());
+            }
+            writer.println("Total: Rp " + cart.getTotalPrice());
+            writer.println("Metode Pembayaran: " + paymentMethod);
+            writer.println("========================\n");
+        } catch (IOException e) {
+            System.out.println("Gagal membuat struk: " + e.getMessage());
         }
-        receipt.append("Total: Rp ").append(cart.getTotalPrice()).append("\n");
-        return receipt.toString();
     }
 }
